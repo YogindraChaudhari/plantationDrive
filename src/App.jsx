241507941608d9d0180900eafea8ce8fa7d97d90
Yogindra <yogindrachaudhari816@gmail.com>
2024-10-27 19:13:51 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,5 @@
-// import React from "react";
-// import {
-//   BrowserRouter as Router,
-//   Route,
-//   Routes,
-//   Navigate,
-// } from "react-router-dom";
-// import { AuthProvider, useAuth } from "./context/AuthContext";
-// import AuthScreen from "./screens/AuthScreen";
-// import HomeScreen from "./screens/HomeScreen";
-// import "./App.css";
-// import RegisterPlant from "./components/RegisterPlant";
-// import DeletePlant from "./components/DeletePlant";
-// import UpdatePlant from "./components/UpdatePlant";
-
-// function App() {
-//   return (
-//     <AuthProvider>
-//       <Router>
-//         <Routes>
-//           {/* Public Routes */}
-//           <Route path="/auth" element={<AuthScreen />} />
-
-//           {/* Protected Routes */}
-//           <Route
-//             path="/"
-//             element={
-//               <PrivateRoute>
-//                 <HomeScreen />
-//               </PrivateRoute>
-//             }
-//           />
-//           <Route path="/register-plant" element={<RegisterPlant />} />
-//           <Route path="/update-plant" element={<UpdatePlant />} />
-//           <Route path="/delete-plant" element={<DeletePlant />} />
-//         </Routes>
-//       </Router>
-//     </AuthProvider>
-//   );
-// }
-
-// // PrivateRoute component to protect routes
-// function PrivateRoute({ children }) {
-//   const { currentUser } = useAuth();
-//   return currentUser ? children : <Navigate to="/auth" />;
-// }
-
-// export default App;
-
-// src/App.js
+import dotenv from "dotenv";
+dotenv.config();
 import React from "react";
 import {
   BrowserRouter as Router,
