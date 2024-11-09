b26bf7d594af0673124c96670d0af52587e4c8ba
Yogindra <yogindrachaudhari816@gmail.com>
2024-11-09 13:46:07 +0530

    removed updated code

--- src/components/MapComponent.jsx
+++ src/components/MapComponent.jsx
@@ -4,7 +4,6 @@ import { db } from "../services/firebaseConfig";
 import { collection, getDocs, Timestamp } from "firebase/firestore";
 import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import "leaflet-color-markers";
-import "./Component.css";
 import L from "leaflet";
 
 // Map zone to marker color icons
@@ -49,13 +48,10 @@ const MapComponent = ({ updateKey }) => {
   const location = useLocation(); // Get location object
   const [imageDate, setImageDate] = useState(null); // State for the image's creation date
   const { latitude, longitude, plantData } = location.state || {};
-  const [loading, setLoading] = useState(true);
 
   const openImageModal = (imageUrl, createdAt) => {
-    if (imageUrl && createdAt) {
-      setSelectedImage(imageUrl);
-      setImageDate(createdAt);
-    } // Set the image's creation date when opening the modal
+    setSelectedImage(imageUrl);
+    setImageDate(createdAt); // Set the image's creation date when opening the modal
   };
 
   const closeImageModal = () => {
@@ -65,12 +61,10 @@ const MapComponent = ({ updateKey }) => {
 
   useEffect(() => {
     const fetchPlants = async () => {
-      setLoading(true);
       const querySnapshot = await getDocs(collection(db, "plants"));
       setPlants(
         querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }))
       );
-      setLoading(false);
     };
     fetchPlants();
   }, [updateKey]); // Refetch when updateKey changes
@@ -87,15 +81,14 @@ const MapComponent = ({ updateKey }) => {
     if (selectedPlant && markerRef.current) {
       setTimeout(() => {
         markerRef.current.openPopup();
-      }, 300); // Add a small delay to ensure the marker is mounted
+      }, 100); // Add a small delay to ensure the marker is mounted
     }
   }, [selectedPlant]);
 
   // Function to format the Firestore timestamp directly
   const formatTimestamp = (timestamp) => {
     if (!timestamp) return "Unknown"; // Return 'Unknown' if no timestamp is available
-    const date =
-      timestamp instanceof Timestamp ? timestamp.toDate() : new Date(timestamp); // Convert Firestore Timestamp to JavaScript Date object
+    const date = timestamp.toDate(); // Convert Firestore Timestamp to JavaScript Date object
     return date.toLocaleString(); // Format the date to a human-readable string
   };
 
@@ -116,127 +109,121 @@ const MapComponent = ({ updateKey }) => {
   return (
     <div className="flex flex-col h-screen relative">
       <div className={`flex-grow ${selectedImage ? "hidden" : ""}`}>
-        {loading ? (
-          <div className="spinner-container">
-            <div className="spinner"></div>
-          </div>
-        ) : (
-          <MapContainer
-            center={[latitude || 19.1, longitude || 73.1]}
-            zoom={10}
-            style={{ height: "100%", zIndex: 0 }}
-          >
-            <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+        <MapContainer
+          center={[latitude || 19.1, longitude || 73.1]}
+          zoom={10}
+          style={{ height: "100%", zIndex: 0 }}
+        >
+          <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
-            {/* Recenter map to selectedPlant location */}
-            {selectedPlant && (
-              <RecenterMap
-                position={[selectedPlant.latitude, selectedPlant.longitude]}
-              />
-            )}
+          {/* Recenter map to selectedPlant location */}
+          {selectedPlant && (
+            <RecenterMap
+              position={[selectedPlant.latitude, selectedPlant.longitude]}
+            />
+          )}
 
-            {plants.map((plant) => (
-              <Marker
-                key={plant.id}
-                position={[plant.latitude, plant.longitude]}
-                icon={createZoneIcon(plant.zone)}
-              >
-                <Popup>
-                  <div className="bg-white shadow-lg rounded-lg p-6 max-w-sm overflow-auto">
-                    <h3 className="text-3xl font-bold mb-2">{plant.name}</h3>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Type:</strong> {plant.type}
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Height:</strong> {plant.height} ft.
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Latitude:</strong> {plant.latitude}
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Longitude:</strong> {plant.longitude}
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Health:</strong> {plant.health}
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Zone:</strong> {plant.zone}
-                    </p>
-                    <p className="text-gray-700 mb-2">
-                      <strong>Plant Number:</strong> {plant.plantNumber}
-                    </p>
-                    <p className="text-gray-700 mb-2">
-                      <strong>Created At:</strong>{" "}
-                      {formatTimestamp(plant.createdAt)}
-                    </p>
-                    {plant.imageUrl && (
-                      <img
-                        src={plant.imageUrl}
-                        alt={plant.name}
-                        className="w-full h-auto rounded-md cursor-pointer"
-                        onClick={() =>
-                          openImageModal(plant.imageUrl, plant.createdAt)
-                        }
-                      />
-                    )}
-                  </div>
-                </Popup>
-              </Marker>
-            ))}
+          {plants.map((plant) => (
+            <Marker
+              key={plant.id}
+              position={[plant.latitude, plant.longitude]}
+              icon={createZoneIcon(plant.zone)}
+            >
+              <Popup>
+                <div className="bg-white shadow-lg rounded-lg p-6 max-w-sm overflow-auto">
+                  <h3 className="text-3xl font-bold mb-2">{plant.name}</h3>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Type:</strong> {plant.type}
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Height:</strong> {plant.height} ft.
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Latitude:</strong> {plant.latitude}
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Longitude:</strong> {plant.longitude}
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Health:</strong> {plant.health}
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Zone:</strong> {plant.zone}
+                  </p>
+                  <p className="text-gray-700 mb-2">
+                    <strong>Plant Number:</strong> {plant.plantNumber}
+                  </p>
+                  <p className="text-gray-700 mb-2">
+                    <strong>Created At:</strong>{" "}
+                    {formatTimestamp(plant.createdAt)}
+                  </p>
+                  {plant.imageUrl && (
+                    <img
+                      src={plant.imageUrl}
+                      alt={plant.name}
+                      className="w-full h-auto rounded-md cursor-pointer"
+                      onClick={() =>
+                        openImageModal(plant.imageUrl, plant.createdAt)
+                      }
+                    />
+                  )}
+                </div>
+              </Popup>
+            </Marker>
+          ))}
 
-            {/* Show popup for the selected plant, if any */}
-            {selectedPlant && (
-              <Marker
-                position={[selectedPlant.latitude, selectedPlant.longitude]}
-                icon={createZoneIcon(selectedPlant.zone, true)}
-                ref={markerRef}
-              >
-                <Popup>
-                  <div className="bg-white shadow-lg rounded-lg p-6 max-w-sm overflow-auto">
-                    <h3 className="text-3xl font-bold mb-2">
-                      {selectedPlant.name}
-                    </h3>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Type:</strong> {selectedPlant.type}
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Height:</strong> {selectedPlant.height} ft.
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Latitude:</strong> {selectedPlant.latitude}
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Longitude:</strong> {selectedPlant.longitude}
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Health:</strong> {selectedPlant.health}
-                    </p>
-                    <p className="text-gray-700 mb-1">
-                      <strong>Zone:</strong> {selectedPlant.zone}
-                    </p>
+          {/* Show popup for the selected plant, if any */}
+          {selectedPlant && (
+            <Marker
+              position={[selectedPlant.latitude, selectedPlant.longitude]}
+              icon={createZoneIcon(selectedPlant.zone, true)}
+              ref={markerRef}
+            >
+              <Popup>
+                <div className="bg-white shadow-lg rounded-lg p-6 max-w-sm overflow-auto">
+                  <h3 className="text-3xl font-bold mb-2">
+                    {selectedPlant.name}
+                  </h3>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Type:</strong> {selectedPlant.type}
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Height:</strong> {selectedPlant.height} ft.
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Latitude:</strong> {selectedPlant.latitude}
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Longitude:</strong> {selectedPlant.longitude}
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Health:</strong> {selectedPlant.health}
+                  </p>
+                  <p className="text-gray-700 mb-1">
+                    <strong>Zone:</strong> {selectedPlant.zone}
+                  </p>
+                  <p className="text-gray-700 mb-2">
+                    <strong>Plant Number:</strong> {selectedPlant.plantNumber}
+                  </p>
+                  {/* Displaying the formatted creation time for selectedPlant */}
+                  {selectedPlant.createdAt && (
                     <p className="text-gray-700 mb-2">
-                      <strong>Plant Number:</strong> {selectedPlant.plantNumber}
+                      <strong>Uploaded On:</strong>{" "}
+                      {formatTimestamp(selectedPlant.createdAt)}
                     </p>
-                    {/* Displaying the formatted creation time for selectedPlant */}
-                    {selectedPlant.createdAt && (
-                      <p className="text-gray-700 mb-2">
-                        <strong>Uploaded On:</strong>{" "}
-                        {formatTimestamp(selectedPlant.createdAt)}
-                      </p>
-                    )}
-                    {selectedPlant.imageUrl && (
-                      <img
-                        src={selectedPlant.imageUrl}
-                        alt={selectedPlant.name}
-                        className="w-full h-auto rounded-md"
-                      />
-                    )}
-                  </div>
-                </Popup>
-              </Marker>
-            )}
-          </MapContainer>
-        )}
+                  )}
+                  {selectedPlant.imageUrl && (
+                    <img
+                      src={selectedPlant.imageUrl}
+                      alt={selectedPlant.name}
+                      className="w-full h-auto rounded-md"
+                    />
+                  )}
+                </div>
+              </Popup>
+            </Marker>
+          )}
+        </MapContainer>
       </div>
 
       {selectedImage && (
